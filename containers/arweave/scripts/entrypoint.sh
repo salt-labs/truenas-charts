e9165752cf9f09f28e1425df57248d68d01f820e
MAHDTech <mahdtech@saltlabs.tech>
2021-09-05 02:36:27 +0000

    Arweave miner container

--- containers/arweave/scripts/entrypoint.sh
+++ containers/arweave/scripts/entrypoint.sh
@@ -15,6 +15,7 @@ export LOGLEVEL="${INPUT_LOGLEVEL:=INFO}"
 export ARWEAVE_HOME="${ARWEAVE_HOME:=/arweave}"
 export ARWEAVE_REWARDS_ADDRESS="${ARWEAVE_REWARDS_ADDRESS}"
 export ARWEAVE_PEERS="peer 188.166.200.45 peer 188.166.192.169 peer 163.47.11.64 peer 139.59.51.59 peer 138.197.232.192"
+export ARWEAVE_CONFIG_DIR="${ARWEAVE_CONFIG_DIR:=$ARWEAVE_HOME/config}"
 export ARWEAVE_DATA_DIR="${ARWEAVE_DATA_DIR:=/data}"
 
 #########################
@@ -33,7 +34,7 @@ checkReqs || { writeLog "ERROR" "Failed to check all requirements" ; exit 1 ; }
 case "${1,,}" in
 
 	version )
-		${ARWEAVE_HOME}/bin/start --"${1}" || { writeLog "ERROR" "Failed to show Arweave version!" ; exit 1 ; }
+		${ARWEAVE_HOME}/bin/arweave --"${1}" || { writeLog "ERROR" "Failed to show Arweave version!" ; exit 1 ; }
 		exit 0
 	;;
 
@@ -51,6 +52,22 @@ esac
 # Check the minimum required variables are populated
 checkVarEmpty "ARWEAVE_REWARDS_ADDRESS" "Arweave Rewards Address" && exit 1
 
-${ARWEAVE_HOME}/bin/start mine mining_addr ${ARWEAVE_REWARDS_ADDRESS} ${ARWEAVE_PEERS} &
+echo -e "##### SYNCING WEAVE #####"
 
-#tail -F 
+${ARWEAVE_HOME}/bin/start \
+	data_dir "${ARWEAVE_DATA_DIR}" \
+	sync_jobs 100 \
+	${ARWEAVE_PEERS} 
+
+echo -e "##### MINING START #####"
+
+${ARWEAVE_HOME}/bin/start \
+	data_dir "${ARWEAVE_DATA_DIR}" \
+	mine \
+	mining_addr ${ARWEAVE_REWARDS_ADDRESS} \
+	${ARWEAVE_PEERS} \
+	&
+
+echo -e "##### TAILING LOGS #####"
+
+${ARWEAVE_HOME}/bin/logs -f
--- containers/arweave/scripts/functions.sh
+++ containers/arweave/scripts/functions.sh
@@ -215,44 +215,50 @@ usage() {
 
 	cat <<- EOF
 
-	# Kaniko
+	Options:
 
-	Flags:
-	    --build-arg multi-arg type                  This flag allows you to pass in ARG values at build time. Set it repeatedly for multiple values.
-	    --cache                                     Use cache when building image
-	    --cache-dir string                          Specify a local directory to use as a cache. (default "/cache")
-	    --cache-repo string                         Specify a repository to use as a cache, otherwise one will be inferred from the destination provided
-	    --cache-ttl duration                        Cache timeout in hours. Defaults to two weeks. (default 336h0m0s)
-	    --cleanup                                   Clean the filesystem at the end
-	-c, --context string                            Path to the dockerfile build context. (default "/workspace/")
-	    --context-sub-path string                   Sub path within the given context.
-	-d, --destination multi-arg type                Registry the final image should be pushed to. Set it repeatedly for multiple destinations.
-	    --digest-file string                        Specify a file to save the digest of the built image to.
-	-f, --dockerfile string                         Path to the dockerfile to be built. (default "Dockerfile")
-	    --force                                     Force building outside of a container
-	-h, --help                                      help for executor
-	    --image-name-with-digest-file string        Specify a file to save the image name w/ digest of the built image to.
-	    --insecure                                  Push to insecure registry using plain HTTP
-	    --insecure-pull                             Pull from insecure registry using plain HTTP
-	    --insecure-registry multi-arg type          Insecure registry using plain HTTP to push and pull. Set it repeatedly for multiple registries.
-	    --label multi-arg type                      Set metadata for an image. Set it repeatedly for multiple labels.
-	    --log-format string                         Log format (text, color, json) (default "color")
-	    --log-timestamp                             Timestamp in log output
-	    --no-push                                   Do not push the image to the registry
-	    --oci-layout-path string                    Path to save the OCI image layout of the built image.
-	    --registry-certificate key-value-arg type   Use the provided certificate for TLS communication with the given registry. Expected format is 'my.registry.url=/path/to/the/server/certificate'.
-	    --registry-mirror string                    Registry mirror to use has pull-through cache instead of docker.io.
-	    --reproducible                              Strip timestamps out of the image to make it reproducible
-	    --single-snapshot                           Take a single snapshot at the end of the build.
-	    --skip-tls-verify                           Push to insecure registry ignoring TLS verify
-	    --skip-tls-verify-pull                      Pull from insecure registry ignoring TLS verify
-	    --skip-tls-verify-registry multi-arg type   Insecure registry ignoring TLS verify to push and pull. Set it repeatedly for multiple registries.
-	    --skip-unused-stages                        Build only used stages if defined to true. Otherwise it builds by default all stages, even the unnecessaries ones until it reaches the target stage / end of Dockerfile
-	    --snapshotMode string                       Change the file attributes inspected during snapshotting (default "full")
-	    --tarPath string                            Path to save the image in as a tarball instead of pushing
-	    --target string                             Set the target build stage to build
-	-v, --verbosity string                          Log level (trace, debug, info, warn, error, fatal, panic) (default "info")
-	    --whitelist-var-run                         Ignore /var/run directory when taking image snapshot. Set it to false to preserve /var/run/ in destination image. (Default true). (default true)
+		config_file (path)                      Load configuration from specified file.
+        peer (ip:port)                          Join a network on a peer (or set of peers).
+        start_from_block_index                  Start the node from the latest stored block index.
+        mine                                    Automatically start mining once the netwok has been joined.
+        port                                    The local port to use for mining. This port must be accessible by remote peers.
+        data_dir                                The directory for storing the weave and the wallets (when generated).
+        metrics_dir                             The directory for persisted metrics.
+        polling (num)                           Poll peers for new blocks every N seconds. Default is 60. Useful in environments where port forwarding is not possible.
+        no_auto_join                            Do not automatically join the network of your peers.
+        mining_addr (addr)                      The address that mining rewards should be credited to. Set 'unclaimed' to send all the rewards to the endowment pool.
+        stage_one_hashing_threads (num)         The number of mining processes searching for the SPoRA chunks to read.Default: 4. If the total number of stage one and stage two processes exceeds the number of available CPU cores, the excess processes will be hashing chunks when anything gets queued, and search for chunks otherwise.
+        io_threads (num)                        The number of processes reading SPoRA chunks during mining. Default: 10.
+        stage_two_hashing_threads (num)         The number of mining processes hashing SPoRA chunks.Default: 6. If the total number of stage one and stage two processes exceeds the number of available CPU cores, the excess processes will be hashing chunks when anything gets queued, and search for chunks otherwise.
+        max_emitters (num)                      The maximum number of transaction propagation processes (default 2).
+        tx_propagation_parallelization (num)    The maximum number of best peers to propagate transactions to at a time (default 4).
+        max_propagation_peers (num)             The maximum number of best peers to propagate blocks and transactions to. Default is 50.
+        sync_jobs (num)                         The number of data syncing jobs to run. Default: 20. Each job periodically picks a range and downloads it from peers.
+        header_sync_jobs (num)                  The number of header syncing jobs to run. Default: 1. Each job periodically picks the latest not synced block header and downloads it from peers.
+        load_mining_key (file)                  Load the address that mining rewards should be credited to from file.
+        ipfs_pin                                Pin incoming IPFS tagged transactions on your local IPFS node.
+        transaction_blacklist (file)            A file containing blacklisted transactions. One Base64 encoded transaction ID per line.
+        transaction_blacklist_url               An HTTP endpoint serving a transaction blacklist.
+        transaction_whitelist (file)            A file containing whitelisted transactions. One Base64 encoded transaction ID per line. If a transaction is in both lists, it is considered whitelisted.
+        transaction_whitelist_url               An HTTP endpoint serving a transaction whitelist.
+        disk_space (num)                        Max size (in GB) for the disk partition containing the Arweave data directory (blocks, txs, etc) when the miner stops writing files to disk.
+        disk_space_check_frequency (num)        The frequency in seconds of requesting the information about the available disk space from the operating system, used to decide on whether to continue syncing the historical data or clean up some space. Default is 30.
+        init                                    Start a new weave.
+        internal_api_secret (secret)            Enables the internal API endpoints, only accessible with this secret. Min. 16 chars.
+        enable (feature)                        Enable a specific (normally disabled) feature. For example, subfield_queries.
+        disable (feature)                       Disable a specific (normally enabled) feature.
+        gateway (domain)                        Run a gateway on the specified domain
+        custom_domain (domain)                  Add a domain to the list of supported custom domains.
+        requests_per_minute_limit (number)      Limit the maximum allowed number of HTTP requests per IP address per minute. Default is 900.
+        max_connections                         The number of connections to be handled concurrently. Its purpose is to prevent your system from being overloaded and ensuring all the connections are handled optimally. Default is 1024.
+        max_gateway_connections                 The number of gateway connections to be handled concurrently. Default is 128.
+        max_poa_option_depth                    The number of PoA alternatives to try until the recall data is found. Has to be an integer > 1. The mining difficulty grows linearly as a function of the alternative as (0.75 + 0.25 * number) * diff, up to (0.75 + 0.25 * max_poa_option_depth) * diff. Default is 500.
+        disk_pool_data_root_expiration_time     The time in seconds of how long a pending or orphaned data root is kept in the disk pool. The default is 2 * 60 * 60 (2 hours).
+        max_disk_pool_buffer_mb                 The max total size in mebibytes of the pending chunks in the disk pool.The default is 2000 (2 GiB).
+        max_disk_pool_data_root_buffer_mb       The max size in mebibytes per data root of the pending chunks in the disk pool. The default is 50.
+        randomx_bulk_hashing_iterations         The number of hashes RandomX generates before reporting the result back to the Arweave miner. The faster CPU hashes, the higher this value should be.
+        disk_cache_size_mb                      The maximum size in mebibytes of the disk space allocated for storing recent block and transaction headers. Default is 5120.
+        debug                                   Enable extended logging.
 
 	EOF
 
